5452d09426c6cdb520dff34c6c89400453730259
Rei2718 <once2718@gmail.com>
2025-09-01 23:54:38 +0900

    zindex

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col justify-end h-[80svh] bg-red-500">
-        <div className="h-[30svh] bg-blue-500"></div>
+      <div className="relative flex flex-col justify-end h-[80svh] bg-red-500 -z-10">
+        <Image
+          src="/hero.webp"
+          alt="Hero Image"
+          layout="fill"
+          objectFit="cover"
+          className="-z-20"
+        />
+        <div className="h-[30svh] bg-blue-500 -z-10"></div>
       </div>
       <div className="w-[80svw] h-[100svh] mx-auto -mt-[15svh] bg-gray-500">
       </div>
